eb4e10476aba95368e90013ea4074d0639d4bdfa
sigitamedingiene <sigitamedingiene@gmail.com>
2022-04-05 19:45:04 +0300

    Somthing wrong. Bool always  returns true.

--- SavaramkiskasDarbas-DataAcsess/CountryRepository.cs
+++ SavaramkiskasDarbas-DataAcsess/CountryRepository.cs
@@ -10,17 +10,38 @@ namespace SavaramkiskasDarbas_DataAcsess
     public class CountryRepository
     {
         private List<Country> countries { get; }
+        private List<Country> countriesEU { get; }
 
         public CountryRepository()
         {
             countries = new List<Country>();
             countries.Add(new Country("LT", "Lietuva"));
             countries.Add(new Country("RU", "Russia"));
+
+            countriesEU = new List<Country>();
+            countriesEU.Add(new Country("LT", "Lietuva"));
+            countriesEU.Add(new Country("LV", "Latvia"));
+            countriesEU.Add(new Country("IS", "Ispanija"));
+            countriesEU.Add(new Country("IT", "Italija"));
+        }
+
+        public bool IsCountryInEurope(string code)
+        {
+            for (int i = 0; i < countriesEU.Count; i++)
+            {
+                if(countriesEU[i].Code == countries[i].Code)
+                {
+                    return true;
+                }                
+            }
+            return false;
         }
 
         public List<Country> Retrieve()
         {
             return countries;
         }
+
     }
+        
 }
--- SavarankiskasDarbas/Program.cs
+++ SavarankiskasDarbas/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using SavaramkiskasDarbas_DataAcsess;
+using SavarankiskasDarbas.Bussnes;
+using System;
+using System.Collections.Generic;
 
 namespace SavarankiskasDarbas
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   CountryRepository countryRepository = new CountryRepository();
+            List<Country> countryList = countryRepository.Retrieve();
+          
+
+            AircraftRepository aircraftRepository = new AircraftRepository();
+            List<Aircraft> aircraftList = aircraftRepository.Retrieve();
+
+            for (int i = 0; i < aircraftList.Count; i++)
+            {
+                Console.Write($"Lektuvo numeris: {aircraftList[i].TailNumber},\r\n lektuvo modelis: {aircraftList[i].Model.Number},\r\n lektuvo kompanija: {aircraftList[i].Company.Name}\r\n");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < aircraftList.Count; i++)
+            {
+                if (countryRepository.IsCountryInEurope(aircraftList[i].Company.Country.Code))
+                {
+                    Console.WriteLine($"Lektuvas Nr:{aircraftList[i].TailNumber} yra Europoje.");
+                } else 
+                {
+                    Console.WriteLine($"Lektuvas Nr:{aircraftList[i].TailNumber} nera Europoje.");
+                }
+            }
+            
         }
     }
 } 
